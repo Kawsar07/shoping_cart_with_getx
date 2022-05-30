f7d20c89a77bef8f95a440191e9e3fa6021e4b5a
Kawsar Ali <59552845+Kawsar07@users.noreply.github.com>
2022-05-30 16:06:56 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:newproject/views/shopping_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ShoppingPage(),
+    );
+  }
+}
